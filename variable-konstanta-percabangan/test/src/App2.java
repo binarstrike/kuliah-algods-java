0a2305b2b703c3527bfc91859846c9e361391b3c
Binar Nugroho <binarnugroho775@gmail.com>
2024-10-15 18:46:16 +0700

    add new project

---
+++ variable-konstanta-percabangan/test/src/App2.java
@@ -0,0 +1,11 @@
+import wkwkw.blah;
+
+public class App2 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        float pi = blah.PI;
+
+        System.out.println("PI is "+pi);
+    }
+}
---
+++ variable-konstanta-percabangan/test/src/wkwkw/blah.java
@@ -0,0 +1,5 @@
+package wkwkw;
+
+public class blah {
+    public static final float PI = 3.14f;
+}
